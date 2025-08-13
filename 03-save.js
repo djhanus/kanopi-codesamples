0202cfb7aaa8e349d79e009d7120055b30647dfd
djhanus <davidjhanus@gmail.com>
2025-08-13 14:54:48 -0400

    add img, alt, doc comments

--- 03-save.js
+++ 03-save.js
@@ -1,5 +1,13 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 
+/**
+ * Save component for testimonial card block
+ * 
+ * @param {Object} props - Component props  
+ * @param {Object} props.attributes - Block attributes
+ * @return {JSX.Element} Save component JSX
+ */
+
 export default function save({ attributes }) {
   const blockProps = useBlockProps.save({
     style: { backgroundColor: attributes.backgroundColor }
@@ -13,6 +21,13 @@ export default function save({ attributes }) {
         value={attributes.testimonialText}
       />
       <div className="author-info">
+        {attributes.authorImage?.url && (
+          <img 
+            src={attributes.authorImage.url} 
+            alt={attributes.authorImage.alt || ''}
+            className="author-image"
+          />
+        )}
         <RichText.Content
           tagName="cite"
           className="author-name"
